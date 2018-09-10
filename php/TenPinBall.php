faeed52e8fe61b3369951084ef63cae3578f287d
Unknown <archit2x3@hotmail.com>
2018-09-10 10:17:55 +0530

    added docs

--- php/TenPinBall.php
+++ php/TenPinBall.php
@@ -4,32 +4,63 @@
  * User: Archit
  * Date: 9/9/2018
  * Time: 7:42 PM
+
+10 pin bowling
+
+1 Game : 10 Frames
+1 Frame : 2 Throws
+
+1-9 Frames :
+max throws 2
+max score 10
+
+10th Frame (special case):
+max throws 3 (if 1st throw is Strike)
+max score 30
+
+valid Frame input [0-10]
+
  */
 
 class TenPinBall
 {
+
     /**
-     * @var
+     * @var i/p's
      */
     protected $gameData;
+    /**
+     * @var array contains errors if any
+     */
     public $errorArray = [];
+    /**
+     * @var array result array
+     */
     public $resultArray = [];
-function __construct($gameData)
+
+    /**
+     * TenPinBall constructor.
+     * @param $gameData
+     */
+    function __construct($gameData)
 {
     $this->gameData = $gameData;
     unset($errorArray);
     unset($resultArray);
 }
 
-function getResults(){
+    /**
+     * @return array|mixed
+     */
+    function getResults(){
     $this->validateInput();
 
     if(isset($this->errorArray[0])){
         $err =  $this->errorArray[0];
-        unset($this->errorArray[0]);
+        unset($this->errorArray[0]); // Empty array for next inputs
         return $err;
 
-       // exit;
+
     }
     else {
         $index=0;
@@ -41,7 +72,12 @@ function getResults(){
     }
 }
 
-private function calculateFrameResult($frame, $index)
+    /**
+     * @param $frame
+     * @param $index
+     * @return int
+     */
+    private function calculateFrameResult($frame, $index)
 {
     if (sizeof($this->resultArray) == 0) {
         $lastElement = 0;
@@ -56,7 +92,13 @@ private function calculateFrameResult($frame, $index)
     }
 }
 
-private function calculateStrike($sFrame, $sIndex, $stmp){
+    /**
+     * @param $sFrame
+     * @param $sIndex
+     * @param $stmp
+     * @return int
+     */
+    private function calculateStrike($sFrame, $sIndex, $stmp){
 if(sizeof($this->gameData[$sIndex+1]) == 1 ){
     $stmp = $stmp + $sFrame;
     return $this->calculateStrike(array_sum($this->gameData[$sIndex+1]),$sIndex+1, $stmp);
@@ -65,9 +107,18 @@ else{
     return $sFrame + array_sum($this->gameData[$sIndex+1]) + $stmp;
 }
 }
-private function validateInput(){
-    if (!is_array($this->gameData)){
 
+    /**
+     * validates input conditions:
+        given input is list of list,
+        size of given data should be 10,
+        1-9 Frame must have size 1 or 2,
+        elements of Frame must be 0 <= element <=10
+        addition must not exceed to 10,
+        special case for 10th Frame
+     */
+    private function validateInput(){
+    if (!is_array($this->gameData)){
             $error = "Invalid Game Data";
             array_push($this->errorArray,$error);
             return;
@@ -84,33 +135,28 @@ private function validateInput(){
         $max = 10;
         if (sizeof($tmparr) == 2) {
             //echo $tmparr[0];
-
             if (!($tmparr[0] >= $min && $tmparr[0] <= $max)) {
-                $error = "sdsadasInvalid Frame Data,  " . $tmparr[0];
+                $error = "sdsadasInvalid Frame Data,  " . json_encode($tmparr);
                 echo (($min <= $tmparr[0]) && ($tmparr[0] <= $max));
-
                 array_push($this->errorArray, $error);
                 return;
             }
             if (!($tmparr[1] >= $min && $tmparr[1] <= $max)) {
-
-                $error = "Invalid Frame Data,  " . $tmparr[1];
+                $error = "Invalid Frame Data,  " . json_encode($tmparr);
                 array_push($this->errorArray, $error);
                 return;
             }
 
         } elseif (sizeof($tmparr) == 1) {
             if ($tmparr[0] != 10) {
-
-                $error = "Invalid Frame Data,  " . $tmparr[0];
+                $error = "Invalid Frame Data,  " . json_encode($tmparr);
                 array_push($this->errorArray, $error);
                 return;
             }
 
-            //var_dump($this->gameData[$i]);
+
         }
         if(array_sum($tmparr) > 10 ){
-
             $error = "Invalid Score, Score should not exceed to 10, current score " . array_sum($tmparr);
             array_push($this->errorArray, $error);
             return;
@@ -118,7 +164,6 @@ private function validateInput(){
     }
     // 10th Frame
     if(sizeof($this->gameData[9]) == 3){
-
         list($v1, $v2, $v3) = $this->gameData[9];
         if($v1 != 10) {
             $error = "Invalid Data, " .$v1 ." Given";
@@ -142,9 +187,7 @@ private function validateInput(){
         $min = 0;
         $max = 10;
 
-
         if (!($v1 >= $min && $v1 <= $max)) {
-
             $error = "Invalid Frame Data,  " . $v1;
             array_push($this->errorArray, $error);
             return;
